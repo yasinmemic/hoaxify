b6338e02fb985fcb0c0db61746476313fb623ad1
Yasinmemic <yasin.memic@hotmail.com>
2020-03-31 13:50:03 +0300

    signup page was developed

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * Created By Yasin Memic on Mar, 2020
@@ -13,11 +16,22 @@ import javax.persistence.Id;
 @Data
 @Entity
 public class User {
+    private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})";
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotNull
+    @Size(min = 4, max = 255)
+    @UniqueUsername
     private String username;
+    @NotNull
+    @Size(min = 4, max = 255)
     private String displayName;
+
+    @Pattern(regexp = PASSWORD_PATTERN)
+    @NotNull
+    @Size(min = 6, max = 255)
     private String password;
 }
+
--- src/main/java/com/hoaxify/ws/user/UserRepository.java
+++ src/main/java/com/hoaxify/ws/user/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created By Yasin Memic on Mar, 2020
  */
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByUsername(String username);
 }
