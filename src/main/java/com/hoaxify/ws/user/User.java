ef35561c5d3e74b8353cf824243482ca4c92a64a
Yasinmemic <yasin.memic@hotmail.com>
2020-03-31 23:53:56 +0300

    adding internationalization

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -2,10 +2,7 @@ package com.hoaxify.ws.user;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -16,21 +13,22 @@ import javax.validation.constraints.Size;
 @Data
 @Entity
 public class User {
-    private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})";
+    private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,255})";
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NotNull
+    @NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
     @Size(min = 4, max = 255)
     @UniqueUsername
+    @Column
     private String username;
-    @NotNull
+    @NotNull(message = "{hoaxify.constraint.displayName.NotNull.message}")
     @Size(min = 4, max = 255)
     private String displayName;
 
-    @Pattern(regexp = PASSWORD_PATTERN)
-    @NotNull
+    @Pattern(regexp = PASSWORD_PATTERN, message = "{hoaxify.constraint.password.Pattern.message}")
+    @NotNull(message = "{hoaxify.constraint.password.NotNull.message}")
     @Size(min = 6, max = 255)
     private String password;
 }
