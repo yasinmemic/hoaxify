3fcd0b17ee5a1acc059162e43a48b31114c99dba
Yasinmemic <yasin.memic@hotmail.com>
2020-05-03 13:13:26 +0300

    Specification was added and some methods have been combined.

--- src/main/java/com/hoaxify/ws/hoax/HoaxRepository.java
+++ src/main/java/com/hoaxify/ws/hoax/HoaxRepository.java
@@ -1,28 +1,26 @@
 package com.hoaxify.ws.hoax;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * Created By Yasin Memic on Apr, 2020
  */
-public interface HoaxRepository extends JpaRepository<Hoax, Long> {
+public interface HoaxRepository extends JpaRepository<Hoax, Long>, JpaSpecificationExecutor<Hoax> {
 
+/*
     Page<Hoax> findAllByUser_Username(String username, Pageable page);
+*/
 
-    Page<Hoax> findByIdLessThan(Long id, Pageable page);
+   /* Page<Hoax> findByIdLessThan(Long id, Pageable page);
 
-    Page<Hoax> findAllByUser_UsernameAndIdLessThan(String username, Long id, Pageable page);
+    Page<Hoax> findAllByUser_UsernameAndIdLessThan(String username, Long id, Pageable page);*/
 
-    Long countByIdGreaterThan(Long id);
+  /*  Long countByIdGreaterThan(Long id);
 
-    Long countByIdGreaterThanAndUser_Username(Long id, String username);
+    Long countByIdGreaterThanAndUser_Username(Long id, String username);*/
 
-    List<Hoax> findByIdGreaterThan(Long id, Sort sort);
+  /*  List<Hoax> findByIdGreaterThan(Long id, Sort sort);
 
-    List<Hoax> findByIdGreaterThanAndUser_Username(Long id, String username, Sort sort);
+    List<Hoax> findByIdGreaterThanAndUser_Username(Long id, String username, Sort sort);*/
 }
